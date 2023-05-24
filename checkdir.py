ef6a07613ba18de73bdc7763b550cb368e77dde3
Gergely Gazda <info@gazdagergo.com>
2023-05-24 19:16:52 +0200

    add exception handling

--- checkdir.py
+++ checkdir.py
@@ -1,12 +1,17 @@
 import os
 
-def check_dir(scheduler, dirname, action, purgeAction=None): 
-  scheduler.enter(5, 1, check_dir, (scheduler, dirname, action, purgeAction))
-  files = os.listdir(dirname)
+def check_dir(scheduler, dirname, action, purgeAction=None):
+  try:
+    scheduler.enter(5, 1, check_dir, (scheduler, dirname, action, purgeAction))
+    files = os.listdir(dirname)
 
-  if len(files) > 0:
-    for file in files:
-      action(file)
+    if len(files) > 0:
+      for file in files:
+        action(file)
 
-      if purgeAction:
-        purgeAction(file)
+        if purgeAction:
+          purgeAction(file)
+  except Exception as e:
+    print(e)
+    pass
+
