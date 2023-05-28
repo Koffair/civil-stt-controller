6b043905a7a9d2ebae5443a539279334de43d28b
Gergely Gazda <info@gazdagergo.com>
2023-05-28 13:48:37 +0200

    feat(process-transcript): add program slug

--- process_transcript.py
+++ process_transcript.py
@@ -2,10 +2,21 @@ import os
 import requests
 
 def process_transcript(audio_id, transcript):
+  program_slug = audio_id.split("_")[0]
   mutation_add_episode = """
-    mutation CreateEpisode($transcript: String) {
-      createEpisode(data: { transcript: $transcript }) {
+    mutation CreateEpisode($transcript: String, $programSlug: String) {
+      createEpisode(data: {
+        transcript: $transcript
+        program: {
+          connect: {
+            slug: $programSlug
+          }
+        }
+      }) {
         id
+        program {
+          id
+        }
       }
     }
   """
@@ -13,8 +24,7 @@ def process_transcript(audio_id, transcript):
   url='https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clfqmnhz406nv01t7fcc9azhz/master'
   token = os.getenv("HYGRAPH_TOKEN")
   headers = {"Authorization": f"Bearer {token}"}
-  variables = {"transcript": transcript}
-
+  variables = {"transcript": transcript, "programSlug": program_slug}
 
   r = requests.post(url, json={"query": mutation_add_episode, "variables": variables}, headers=headers)
   json_data = r.json()
