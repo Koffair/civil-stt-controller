a675bc6796399300ab34938f055cda35ef7eedeb
Gergely Gazda <info@gazdagergo.com>
2023-09-05 12:24:59 +0200

    Revert "kg/issue_01"

--- graphql_service.py
+++ graphql_service.py
@@ -1,69 +1,36 @@
 import os
 import requests
-import exceptions
 
 def mutate(mutation, variables):
   url=os.getenv('HYGRAPH_URL')
   token = os.getenv("HYGRAPH_TOKEN")
   headers = {"Authorization": f"Bearer {token}"}
 
-  try:
-    r = requests.post(url, json={"query23515": mutation, "variables": variables}, headers=headers)
-    json_data = r.json()
-
-    r.raise_for_status()
-    return json_data
-  
-  except requests.exceptions.HTTPError as error: 
-    raise exceptions.CustomException("HTTPError error " + str(error))
-  
-  except requests.exceptions.ConnectionError as error:
-    raise exceptions.CustomException("ConnectionError" + str(error))
-  
-  except requests.exceptions.Timeout as error:
-    raise exceptions.CustomException("Timeout error" + str(error))
-  
-  except requests.exceptions.TooManyRedirects as error:
-    raise exceptions.CustomException("TooManyRedirects error" + str(error))
-  
-  except requests.exceptions.RequestException as error:
-    raise exceptions.CustomException("RequestException(catastrophic) error" + str(error))
+  r = requests.post(url, json={"query": mutation, "variables": variables}, headers=headers)
+  json_data = r.json()
+  # print(json_data)
+  return json_data
 
 
 def create_episode(audio_id, program_slug, release_date, audio_url, transcript = ""):
   mutation = """
-    mutation upsertEpisode(
+    mutation CreateEpisode(
       $transcript: String,
       $programSlug: String,
       $releaseDate: Date,
       $audioUrl: String,
       $audioId: String
     ) {
-      upsertEpisode(
-        where:{ audioId: $audioId },
-        upsert: {
-          create: {
-            transcript: $transcript
-            program: {
-              connect: {
-                slug: $programSlug
-              }
-            }
-            releaseDate: $releaseDate,
-            audioUrl: $audioUrl,
-            audioId: $audioId
-          },
-          update: {
-            transcript: $transcript
-            program: {
-              connect: {
-                slug: $programSlug
-              }
-            }
-            releaseDate: $releaseDate,
-            audioUrl: $audioUrl,
-            audioId: $audioId
+      createEpisode(data: {
+        transcript: $transcript
+        program: {
+          connect: {
+            slug: $programSlug
           }
+        }
+        releaseDate: $releaseDate,
+        audioUrl: $audioUrl,
+        audioId: $audioId
       }) {
         id
         program {
@@ -115,17 +82,10 @@ def update_transcript(audio_id, transcript):
   return mutate(mutation, variables)
 
 def gql_create_episode_and_publish(audio_id, program_slug, release_date, audio_url, transcript = ""):
-  try:
-    create_episode(audio_id, program_slug, release_date, audio_url, transcript)
-    publish_episode(audio_id)
-  except Exception as error:
-    raise exceptions.CustomException(str(error))
+  create_episode(audio_id, program_slug, release_date, audio_url, transcript)
+  publish_episode(audio_id)
 
 def gql_update_episode_transcript_and_publish(audio_id, transcript):
-  try:
-    update_transcript(audio_id, transcript)
-    publish_episode(audio_id)
-    print('gql_update_episode_transcript_and_publish')
-  except Exception as error:
-    print(error)
-    raise exceptions.CustomException(str(error))
+  update_transcript(audio_id, transcript)
+  publish_episode(audio_id)
+  print('gql_update_episode_transcript_and_publish')
--- outputwatch.py
+++ outputwatch.py
@@ -6,7 +6,6 @@ from checkdir import check_dir
 from functions import get_audio_metadata
 from graphql_service import gql_update_episode_transcript_and_publish
 from db_service import update_transcript_in_db
-from file_service import move_or_copy_file
 
 load_dotenv()
 
@@ -22,31 +21,14 @@ def process_file(file):
   if file.endswith('.txt'):
     audio_id, program_slug, release_date, audio_url = get_audio_metadata(file)
     transcript = read_textfile(file)
-    error_message = ''
-    try:
-      gql_update_episode_transcript_and_publish(audio_id, transcript=transcript)
-
-    except Exception as error:
-      print("gql update was not succesful, error: ", error)
-      move_or_copy_file(os.getenv('PROCESSING_FOLDER'), os.getenv('ERROR_FOLDER'), audio_id + '.mp3', True)
-      move_or_copy_file(os.getenv('OUTPUT_FOLDER'), os.getenv('ERROR_FOLDER'), file, True)
-      error_message=str(error)
-
-    update_transcript_in_db(audio_id, transcript, error_message=error_message)
-
-def delete_from_processed(file):
-  audio_id, program_slug, release_date, audio_url = get_audio_metadata(file)
-  os.remove(os.getenv('PROCESSING_FOLDER') + '/' + audio_id + '.mp3')
-  print(file, 'deleted from processing folder')
-
+    update_transcript_in_db(audio_id, transcript)
+    gql_update_episode_transcript_and_publish(audio_id, transcript=transcript)
 
 def remove_text_file(file):
   if file.endswith('.txt'):
-    delete_from_processed(file)
     src_fpath = os.getenv('OUTPUT_FOLDER') + '/' + file
     os.remove(src_fpath)
     print(file, 'transcript text file removed')
-
 
 my_scheduler = sched.scheduler(time.time, time.sleep)
 my_scheduler.enter(5, 1, check_dir, (my_scheduler, os.getenv('OUTPUT_FOLDER'), process_file, remove_text_file))
